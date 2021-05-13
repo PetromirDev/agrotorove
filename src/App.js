f2f46bf58f054c1ab1b892ee9cf10f12e9a188e6
PGameplay <petromir_petrov98@mail.bg>
2021-05-13 12:15:16 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import {  BrowserRouter as Router,
+  Switch,
+  Route} from 'react-router-dom'
+import Home from './pages/Home';
+import phone from "./images/phone.png"
+import email from './images/email.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div className="navWrapper">
+      <div className="topNav">
+        <h4>Използвайте качествена тор!</h4>
+        <div className="phoneWrapper">
+          <img src={email} alt="email-icon" width="30" height="30"/>
+          <h4>agrotorove77@abv.bg</h4>
+          <img src={phone} alt="phone-icon" width="30" height="30"/>
+          <h4>+ 359 887 697 749</h4>
+        </div>
+      </div>
+      <div className="bottomNav">
+        <div className="logoWrapper">
+          <h1>АГРО ТОРОВЕ 77</h1>
+        </div>
+        <div className="navigation">
+          <div className="navItem">
+            <a href="/">Начало</a>
+          </div>
+          <div className="navItem">
+            <a href="/torove">Торове</a>
+          </div>          
+          <div className="navItem">
+            <a href="/about">За Нас</a>
+          </div>
+          <div className="navItem">
+            <a href="/contact-us">Контакти</a>
+          </div>          
+        </div>
+      </div>
     </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+            <Home/>
+        </Route>
+        <Route exact path="/torove">
+          <h1>TOROVE</h1>
+        </Route>
+        <Route path="*">
+          <div style={{height:'89.5vh', display:'flex',alignItems:'center',justifyContent:'center'}}>
+            <h1>Страницата не е намерена :(</h1>
+          </div>
+          
+        </Route>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Landing.css'
+
+export default function Home() {
+    return (
+        <div className="landingBG">
+            <div className="welcomeMessage">            
+                <h1>МИНЕРАЛНИ ТОРОВЕ</h1>
+                <div className="bottom"></div>
+                <h2>Кратък лозунг</h2>
+                <button>Прочети повече</button>
+            </div>
+
+        </div>
+    )
+}
